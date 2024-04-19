7e1d8c76d09d7aa0b09b8a6a70bec47643bf0dcf
Dale Wyttenbach <dale.wyttenbach@gmail.com>
2024-04-19 14:50:13 -0500

    good

--- python/src/modules.py
+++ python/src/modules.py
@@ -4,6 +4,8 @@ def makePlayer(name, age, **dict):
     player={}
     player['name'] = name
     player['age'] = age
+    for k, v in dict.items():
+        player[k] = v
     return player
 
-print(makePlayer(name='JONES', age=29))
+print(makePlayer(name='JONES', age=29, drink='bourbon'))
