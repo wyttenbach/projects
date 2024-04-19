78d0705c3d5ccecbce70fc5c7651b76368357be9
Dale Wyttenbach <dale.wyttenbach@gmail.com>
2024-04-18 19:21:49 -0500

    good

--- connect/unpack-test.sh
+++ connect/unpack-test.sh
@@ -3,4 +3,4 @@ file=ViersD2D526EZFormService_V6_v5_1_1_0_20230808_032741.bar
 if [ $# -gt 0 ]; then
     file="$1"
 fi
-/bin/rm -rf BAR_IMAGE/ && unpack.sh $file
+/bin/rm -rf BAR_IMAGE BAR-META-INF bar.properties && unpack.sh $file
