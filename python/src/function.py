5b776331ceb2a9259de84acd8596c34b6b367264
Dale Wyttenbach <dale.wyttenbach@gmail.com>
2024-04-19 13:36:05 -0500

    Dale Evans

--- python/src/function.py
+++ python/src/function.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
-def multiply(a = 2, b = 3):
-    result = a * b
-    return result
+# def multiply(a = 2, b = 3):
+#     result = a * b
+#     return result
 
-# returnedValue = multiply(2, 3)
-print(multiply())
+# # returnedValue = multiply(2, 3)
+# print(multiply())
+
+def printName(firstname='Dale', lastname='Evans'):
+    print(firstname.title(), lastname.title())
+
+printName()
