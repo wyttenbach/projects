4579db431cce061f294964cfec9b17ebcb390688
Dale Wyttenbach <dale.wyttenbach@va.gov>
2024-06-24 10:24:33 -0500

    maven profile va

--- va/hook.sh
+++ va/hook.sh
@@ -1,4 +1,4 @@
 # source this file, don't execute it
 . ~/bin/java_home.sh $JAVA21
-
+export MAVEN_PROFILE=va
 export PPFIND_EXCLUDES="grep -v src/main/web"
--- va8/hook.sh
+++ va8/hook.sh
@@ -1,5 +1,6 @@
 # source this file, don't execute it
 . ~/bin/java_home.sh $JAVA8
+# JAVA8 because: wsimport is \tools\jdk8_202/bin/wsimport
 
-# because: wsimport is \tools\jdk8_202/bin/wsimport
+export MAVEN_PROFILE=va
 
